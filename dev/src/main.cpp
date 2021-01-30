cc21589298b6fc9e140f7698daaa493d1f032c8e
ce shen <scbebetterme@gmail.com>
2021-01-30 21:40:03 +0800

    add arnoldi

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -52,29 +52,28 @@ ITensor my_extract_it(arma::mat& M) {
 #include "itensor/all.h"
 
 int main() {
-	int N = 8;
-	auto sites = SpinHalf(N);
-	auto indices = sites.inds();
-
-	//auto psi = randomITensor(QN({ "Sz",0 }), indices);
-
-	auto ampo = AutoMPO(sites);
-	for (auto j : range1(N - 1))
-	{
-		ampo += -4, "Sz", j, "Sz", j + 1;
-		ampo += -2, "Sx", j;
+	const double beta_c = 0.5 * log(1 + sqrt(2));
+	ITensor A0 = database::ising2d(beta_c);
+	int len = 3;
+	ITensor M = glue_bare_ring(A0, len);
+	Print(M);
+	//M.noPrime();
+	for (auto i = 0; i < len; i++) {
+		M.replaceTags("u," + str(i), "1,s=" + str(i));
+		M.replaceTags("d," + str(i), "0,s=" + str(i));
 	}
-	ampo += -4, "Sz", N, "Sz", 1;
-	ampo += -2, "Sx", N;
-	//ampo += "Sz", N, "Sz", 1; //periodic boundary condition?
-	auto Hmpo = toMPO(ampo);
+	//M.replaceTags("u,0", "1");
+	//M.replaceTags("d,0", "0");
+	Print(M);
+	/*IndexSet u_is = findInds(M, "1");
+	IndexSet d_is = findInds(M, "0");
+	auto [uT, U] = combiner(u_is);
+	auto [dT, D] = combiner(d_is);*/
 
-	auto H = Hmpo(1);
-	for (auto j : range1(2, N)) H *= Hmpo(j);
+	auto [U, D] = diagHermitian(M);
+	PrintData(D);
+	PrintData(U);
 
-	H = removeQNs(H);
-	Print(H);
-
-	auto [U, D] = eigen(H);
+	
 }
 
