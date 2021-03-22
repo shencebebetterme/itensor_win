6137a7534ce0b16e2734a4beaf4fd3299c62673a
ce shen <scbebetterme@gmail.com>
2021-03-22 19:22:34 +0800

    adjoint.h to diagonalize the adjoint operator

---
+++ dev/src/sample/adjoint.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "pch.h"
+
+#include "inc.h"
+#include "util/database.h"
+#include "util/extract_mat.h"
+#include "util/cft_data.h"
+#include "util/glue.h"
+#include "util/it_help.h"
+
+#include "util/arpack_wrap.h"
+
