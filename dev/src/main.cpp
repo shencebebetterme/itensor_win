0bc791fa114191f6ba516272d6a10afb36444f2b
ce shen <scbebetterme@gmail.com>
2021-04-13 21:09:59 +0800

    1d ising spectrum by diagonalizing MPO

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -18,19 +18,13 @@ using namespace std::chrono;
 
 //ITensor trg(ITensor, int, int);
 
-
-
-
-
 //#include "sample/ctmrg.h"
 //#include "itensor/all.h"
 //#include "util/arnoldi.h"
-// 
+//
 #include "util/arpack_wrap.h"
 #include "sample/arpack_test.h"
 //#include "sample/adjoint.h"
-
-
 
 template<typename T>
 class MyClass
@@ -41,11 +35,8 @@ public:
 		x_ = x;
 	}
 
-
 private:
-
 };
-
 
 template<typename T, typename T2>
 T2 f(MyClass<T>& mc_) {
@@ -55,42 +46,7 @@ T2 f(MyClass<T>& mc_) {
 }
 
 
-
-
-int main(int argc, char** argv) {
-
-	//dmrg_ising();
-	//dmrg_ising_excited();
-
-	/*std::vector<int> v1 = { 3,8,7,9 };
-	std::vector<std::string> v2 = { "h","e","l","o" };
-	ssort(v1, v2);*/
-
-	//arpack_test2();
-
-	//const double beta_c = 0.5 * log(1 + sqrt(2));
-	//ITensor A0 = database::ising2d(beta_c);
-
-	////Todo: extract L0 + L0bar from transfer matrix
-	//int n_chain = 4;
-	//const double c_ising = 0.5;//central charge
-	//const double f_ising = 0.929695;//free energy per site
-	//const double fA = f_ising * 2; //free energy per A tensor
-
-	//A0 /= fA;
-	//ITensor M = glue_bare_ring(A0, n_chain);
-
-	//double factor = std::exp((2 * PI / n_chain) * (c_ising / 12) + n_chain * fA);
-	//M /= factor;
-	//
-	////todo: how to check that M is nearly identity?
-
-	//adtest2();
-	//arpack_test2();
-	//arpack_test();
-
-
-	//todo: L0 + L0bar from H of 1d ising loop
+void check_ising_spectrum() {
 	int N = 12;
 	//int N = std::atoi(argv[1]);
 	//auto sites = SpinHalf(N);
@@ -124,8 +80,8 @@ int main(int argc, char** argv) {
 
 	HT *= 0.5; // now HT should have the same spectrum as L0 + L0bar with a shift
 
-	//todo: check spectrum of HT
-	IndexSet HTis = findInds(HT, "0"); 
+	//done: check spectrum of HT
+	IndexSet HTis = findInds(HT, "0");
 	auto HTM = MyITensorMap(HTis);
 	HTM.A_ = HT;
 
@@ -141,10 +97,81 @@ int main(int argc, char** argv) {
 
 	printfln("relative energy spectrum is");
 	for (int i = 0; i < nev; i++) {
-		std::cout << eigval[i]- eigval[0] << std::endl;
+		std::cout << eigval[i] - eigval[0] << std::endl;
 		//PrintData(eigvecs[i]);
 	}
+}
+
+
+//MPO chain, with pairs of unprimed and primed indices
+class MPOMap : public ITensorMapBase {
+public:
+	MPO mpo;
+	int N_ = 1;// will be set to the length of mpo
+
+	// pass the default constructor
+	MPOMap(IndexSet& is)
+		: ITensorMapBase(is)
+	{}
+
+	void product(ITensor const& x, ITensor& y) const
+	{
+		//y = A_ * x;
+		//replace all "u" tags to "d" tags
+		y = mpo.A(1) * x;
+		for (int i = 2; i <= N_; i++) {
+			y *= mpo.A(i);
+		}
+		y.noPrime();
+	}
+};
+
+
+void check_ising_mpo_spectrum() {
+	int N = 16;
+	auto sites = SpinHalf(N, { "ConserveQNs=",false });
+
+	double h = 1.0; //at critical point
+
+	auto ampo = AutoMPO(sites);
+
+	for (int j = 1; j < N; ++j)
+	{
+		ampo += -4.0, "Sz", j, "Sz", j + 1;
+	}
+
+	ampo += -4.0, "Sz", 1, "Sz", N;
+
+	for (int j = 1; j <= N; ++j) {
+		ampo += -2.0 * h, "Sx", j;
+	}
+
+	auto H = toMPO(ampo);
+
+	IndexSet HTis = sites.inds();
+
+	MPOMap mmap(HTis);
+	mmap.mpo = H;
+	mmap.N_ = N;
+
+
+	int nev = 10;
+	std::vector<Cplx> eigval = {};
+	std::vector<ITensor> eigvecs = {};
+	itwrap::eig_arpack<double>(eigval, eigvecs, mmap, { "nev=",nev,"tol=",1E-8, "ReEigvec=",true,"WhichEig=","SR" });
+
+	printfln("normalized relative energy spectrum is");
+	for (int i = 0; i < nev; i++) {
+		double ei = real(eigval[i] - eigval[0]) * N / (2 * PI);
+		ei *= 0.5; // remove an extra factor of 2
+		std::cout << ei << std::endl;
+		//PrintData(eigvecs[i]);
+	}
+}
+
+
+int main(int argc, char** argv) {
+	
 
 	int a = 0;
 }
-
