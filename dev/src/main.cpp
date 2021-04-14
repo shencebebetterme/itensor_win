c23de6caf876b016ac3bba324f53a1099e5b37af
ce shen <scbebetterme@gmail.com>
2021-04-14 14:03:11 +0800

    diagonalize ad_H

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -148,6 +148,9 @@ void check_ising_mpo_spectrum() {
 
 	auto H = toMPO(ampo);
 
+	H *= N / (2 * PI);
+	H *= 0.5;// remove an extra factor of 2
+
 	IndexSet HTis = sites.inds();
 
 	MPOMap mmap(HTis);
@@ -155,23 +158,140 @@ void check_ising_mpo_spectrum() {
 	mmap.N_ = N;
 
 
-	int nev = 10;
+	int nev = 20;
 	std::vector<Cplx> eigval = {};
 	std::vector<ITensor> eigvecs = {};
 	itwrap::eig_arpack<double>(eigval, eigvecs, mmap, { "nev=",nev,"tol=",1E-8, "ReEigvec=",true,"WhichEig=","SR" });
 
+	/*for (int i = 0; i < nev; i++) {
+		std::cout << eigval[i] << std::endl;
+	}*/
+
+	printfln("ground state energy is %.10f", eigval[0]);
+
 	printfln("normalized relative energy spectrum is");
 	for (int i = 0; i < nev; i++) {
-		double ei = real(eigval[i] - eigval[0]) * N / (2 * PI);
-		ei *= 0.5; // remove an extra factor of 2
+		double ei = real(eigval[i] - eigval[0]);
 		std::cout << ei << std::endl;
 		//PrintData(eigvecs[i]);
 	}
+
 }
+
+
+//contract a MPO to an ITensor
+//n is the number of shared indices between M and A
+ITensor cMT(MPO M, ITensor A, int n) {
+	ITensor res = M.A(1) * A;
+	for (int i = 2; i <= n; ++i) {
+		res *= M.A(i);
+	}
+	return res;
+}
+
+
+class adMPOMap : public ITensorMapBase {
+public:
+
+	MPO Z_; // to implement ad_Z
+	int ldim_; // dim of each leg
+	int nl_; // Z has 2*nl legs
+	IndexSet uis;
+	IndexSet dis;
+	IndexSet tis; //temporary index sets
+
+	// inds(Z_) = inds(x) = is = {u, u', u'', ... d, d', d'', ...}
+	adMPOMap(IndexSet& is)
+		: ITensorMapBase(is)
+	{}
+
+
+	// ad_Z maps X to [Z,X]
+	void ad(ITensor const& x, ITensor& y) const
+	{
+		// y = Z x - x Z
+		// suppose x and Z has the same indexing structure
+		// with indices u1, u2, u3, ... and u1', u2', u3', ...
+		// x is an itensor, z is a MPO
+		
+
+		MPO Zc = prime(Z_);
+		ITensor xc = prime(x);
+		y = cMT(Zc, x, nl_) - cMT(Z_, xc, nl_); 
+		//Zc.replaceTags("u", "uu").replaceTags("d", "u");
+		//xc.replaceTags("u", "uu").replaceTags("d", "u");
+		//y = Zc * x - xc * Z_;
+		y.replaceTags("2", "1");//now y has indices u1, u2, u3, ... and u1', u2', u3', ...
+	}
+
+	// ad^2
+	void product(ITensor const& x, ITensor& y) const
+	{
+		ITensor tmp = x;
+		ad(x, tmp);
+		ad(tmp, y);
+		//y *= -1;
+	}
+};
 
 
 int main(int argc, char** argv) {
 	
+	int N = 7;
+	auto sites = SpinHalf(N, { "ConserveQNs=",false });
 
-	int a = 0;
+	double h = 1.0; //at critical point
+
+	auto ampo = AutoMPO(sites);
+
+	for (int j = 1; j < N; ++j)
+	{
+		ampo += -4.0, "Sz", j, "Sz", j + 1;
+	}
+
+	ampo += -4.0, "Sz", 1, "Sz", N;
+
+	for (int j = 1; j <= N; ++j) {
+		ampo += -2.0 * h, "Sx", j;
+	}
+
+	auto H = toMPO(ampo);
+
+	// scale H to L0 + L0bar
+	H *= N / (2 * PI);
+	H *= 0.5; // remove an extra factor of 2
+	//todo: shift H s.t. its lowest eigenvalue is 0
+	IndexSet HTis = sites.inds();
+
+	MPOMap mmap(HTis);
+	mmap.mpo = H;
+	mmap.N_ = N;
+
+	int nev = 2;
+	std::vector<Cplx> eigval = {};
+	std::vector<ITensor> eigvecs = {};
+	itwrap::eig_arpack<double>(eigval, eigvecs, mmap, { "nev=",nev,"tol=",1E-8, "ReEigvec=",true,"WhichEig=","SR" });
+	double gse = eigval[0].real();
+
+	//IndexSet HDis = sites.inds();
+	//IndexSet HUis = sites.inds().prime();
+	// IndexSet HTis = HDis + HUis
+	IndexSet HTis = unionInds(sites.inds(), sites.inds().prime());
+	
+	adMPOMap admap(HTis);
+	admap.Z_ = H;
+	admap.nl_ = N;
+
+	int nev = 10;
+	std::vector<Cplx> eigval = {};
+	std::vector<ITensor> eigvecs = {};
+	itwrap::eig_arpack<double>(eigval, eigvecs, admap, { "nev=",nev,"tol=",1E-8, "ReEigvec=",true,"WhichEig=","SR" });
+
+	printfln("first eigenvalue is %.10f", eigval[0]);
+
+	for (int i = 0; i < nev; i++) {
+		double ei = real(eigval[i] - eigval[0]);
+		std::cout << ei << std::endl;
+		//PrintData(eigvecs[i]);
+	}
 }
